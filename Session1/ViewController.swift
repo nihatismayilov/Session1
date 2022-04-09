f3ede0abaf979b4c254aab31842d34a200ecb2a4
nihatismayilov <nihatismayilov46@gamil.com>
2022-04-09 16:12:13 +0400

    first task

--- Session1/ViewController.swift
+++ Session1/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Nihad Ismayilov on 09.04.22.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.frame = CGRect.init(x: 100, y: 100, width: 100, height: 30)
+        label.text = "Test, trial"
+        label.textColor = .black
+        
+        self.view.addSubview(label)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print(#function)
     }
 
 
