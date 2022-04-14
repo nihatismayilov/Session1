b1803fcf4be5eaee12a8f1c544cdb9f5234b6a06
nihatismayilov <nihatismayilov46@gamil.com>
2022-04-14 22:37:06 +0400

    first task

--- Session1/ViewController.swift
+++ Session1/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
         label.textColor = .black
         
         self.view.addSubview(label)
+        
+        task2()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +36,39 @@ class ViewController: UIViewController {
         print(#function)
     }
 
+}
+
+func task2() {
+    let bestFriend = "Chingiz"
+    print(bestFriend)
+
+    let myName = "Nihad"
+    let myAddress = "Bineqedi"
+
+    print(myName)
+    print(myAddress)
+
+    let randomNum = Int.random(in: 10...20)
+
+    print(Double(randomNum)/4)
+
+    let x = Int.random(in: 0...100)
+
+    print(x)
+
+    print("The number smaller by 22 is: \(x - 22)")
+    print("The number bigger by 22 is: \(x + 22)")
+
+    let reminder = x % 2
+
+    print("the reminder of the division by 2 iS \(reminder)")
+
+    let num1 = Int.random(in: 0...100)
+    let num2 = Int.random(in: 0...100)
+    let num3 = Int.random(in: 0...100)
+    let sum = num1 + num2 + num3
+
+    print("the sum of 3 random numbers is \(sum)")
 
 }
 
